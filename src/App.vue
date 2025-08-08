dcbf21d194ee9c62fc66962890e40bd2bc1d32a0
Di Jiang <djia0019@student.monash.edu>
2025-08-08 20:20:41 +1000

    add myname.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
+<!-- src/App.vue -->
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import MyName from './components/MyName.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,6 +22,9 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
+
+    <!-- 直接插入你的 MyName 组件 -->
+    <MyName />
   </main>
 </template>
 
---
+++ src/components/MyName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-name">
+    Hello, I'm Di Jiang
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+.my-name {
+  font-size: 1.2rem;
+  color: #2c3e50;
+  margin-top: 1rem;
+}
+</style>
